bd510cb1ccb8316d5d8bf264ad6f4490cd80ed60
Mama679 <ingmariomanotas@hotmail.com>
2023-08-11 10:49:33 -0500

    Autenticación api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\BookController as Bookv1;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login',[LoginController::class,'login']);
+
+Route::apiResource('v1/books',Bookv1::class)
+->only(['index','show','destroy'])
+->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
