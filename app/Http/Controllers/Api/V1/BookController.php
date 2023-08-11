e840e7a1d55153972d4a35e8a189d4c1017db44b
Mama679 <ingmariomanotas@hotmail.com>
2023-08-11 11:15:54 -0500

    Agrego metodo all, al obtener todos los registro

--- app/Http/Controllers/Api/V1/BookController.php
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -16,7 +16,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        return BookResource::collection(Book::paginate());
+        return BookResource::collection(Book::all());
     }
 
     /**
