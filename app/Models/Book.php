a3fbb6329522e4bde4e0299241fd5b3bc1812113
Mama679 <ingmariomanotas@hotmail.com>
2023-08-11 09:43:17 -0500

    Estructura de Base de Datos

---
+++ app/Models/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillAble = [
+        'title',
+        'description',
+        'user_id'
+    ];
+
+    public function getDescription($value){
+        return substr($value,1,120);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
